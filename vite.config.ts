07eca8a5df8dc6dd0c8fb9ae19b50b27ee9491ae
rishiotbt <ritesh_p@optimusbt.net>
2025-09-24 13:18:00 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/vendorproject/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
